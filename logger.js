0601af4d4c4cb47402896c8b56eaaa9bac936714
Vera Kahn <verakahn@nypl.org>
2022-02-17 11:41:24 -0500

    update logger and env test

--- logger.js
+++ logger.js
@@ -79,7 +79,6 @@ const logger = winston.createLogger({
  *  Set logging level (e.g. 'debug','info','warn','error','critical')
  */
 logger.setLevel = (level) => {
-  logger.transports.console.level = level
+  logger.transports.forEach((transport) => transport.level = level)
 }
-
 module.exports = logger
--- test/installation-test/test-with-env-creds.js
+++ test/installation-test/test-with-env-creds.js
@@ -6,12 +6,14 @@
  *   SIERRA_SECRET=...
  *   SIERRA_BASE=https://.../v6/
  *
- *  source .env; node ./test-with-env-creds
+ *  source .env && node ./test-with-env-creds
  */
-const wrapper = require('@nypl/sierra-wrapper')
+// const wrapper = require('@nypl/sierra-wrapper')
+const wrapper = require('../../index')
+
 
 // It would be nice to be able to set logLevel as follows:
-wrapper.config({ logLevel: 'debug' })
+wrapper.setLogLevel('error')
 
 const run = async () => {
   const bibs = await wrapper.get('bibs')
