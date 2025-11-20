ead2b5818fa640d52c859536409f4e0b045dbd04
danamansana <danielappel@nypl.org>
2025-11-20 14:06:27 -0500

    Fix mocking in tests

--- test/index.test.js
+++ test/index.test.js
@@ -21,9 +21,9 @@ describe('test', function () {
   let mockAxios
   beforeEach(function () {
     wrapper = rewire('../index.js')
-    mockAxios = new MockAdapter(axios)
     wrapper.config({ key: credsKey, secret: credsSecret, base: credsBase })
   })
+
   describe('authenticate', function () {
     it('should throw an error if there are no credentials', async function () {
       const wrapper = requireUncached('../index.js')
@@ -31,51 +31,77 @@ describe('test', function () {
     })
 
     it('should make an axios post request with the credentials', async function () {
-      mockAxios.onPost(credsBase + 'token')
-        .reply(200, { access_token: '12345' })
+
+      wrapper.__set__('fetch', (path, options) => {
+        if (path === credsBase + 'token' && options.headers.Authorization === 'Basic Y3JlZHNLZXk6Y3JlZHNTZWNyZXQ=') {
+          return { status: 200, json: () => { return { access_token: '12345' } } }
+        }
+      })
 
       await wrapper.authenticate()
       expect(wrapper._accessToken()).to.equal('12345')
     })
+
     it('retries authentication 1x when given an empty response once', async () => {
-      mockAxios.onPost(credsBase + 'token')
-        .replyOnce(200, '')
-        .onPost(credsBase + 'token')
-        .replyOnce(200, { access_token: '12345' })
       wrapper = rewire('../index.js')
       wrapper.config({ key: credsKey, secret: credsSecret, base: credsBase })
       const retryAuth = wrapper.__get__('_retryAuth')
       const retrySpy = sinon.spy(retryAuth)
       wrapper.__set__('_retryAuth', retrySpy)
+      let tries = 0
+      wrapper.__set__('fetch', (path, options) => {
+        if (path === credsBase + 'token' && tries !== 0 && options.headers.Authorization === 'Basic Y3JlZHNLZXk6Y3JlZHNTZWNyZXQ=') {
+          return { status: 200, json: () => { return { access_token: '12345' } } }
+        } else {
+          tries += 1
+          return { status: 200, json: () => '' }
+        }
+      })
 
       await wrapper.authenticate()
 
       sinon.assert.calledOnce(retrySpy)
     })
+
     it('logs a warning when there is an empty response', async () => {
       const loggerWarning = sinon.spy(logger, 'warning')
-      mockAxios.onPost(credsBase + 'token')
-        .replyOnce(200, '')
-        .onPost(credsBase + 'token')
-        .replyOnce(200, { access_token: '12345' })
+      let tries = 0
+      wrapper = rewire('../index.js')
+      wrapper.config({ key: credsKey, secret: credsSecret, base: credsBase })
+      wrapper.__set__('fetch', (path, options) => {
+        if (path === credsBase + 'token' && tries !== 0 && options.headers.Authorization === 'Basic Y3JlZHNLZXk6Y3JlZHNTZWNyZXQ=') {
+          return { status: 200, json: () => { return { access_token: '12345' } } }
+        } else {
+          tries += 1
+          return { status: 200, json: () => '' }
+        }
+      })
       await wrapper.authenticate()
 
       sinon.assert.calledOnce(loggerWarning)
 
       logger.warning.restore()
     })
+
     it('Throws a RetryError when auth fails 3 times', async () => {
-      mockAxios.onPost(credsBase + 'token')
-        .reply(200, '')
+      wrapper = rewire('../index.js')
+      wrapper.config({ key: credsKey, secret: credsSecret, base: credsBase })
+      wrapper.__set__('fetch', (path, options) => {
+        return { status: 200, json: () => '' }
+      })
       const errorMessage = 'Authentication failed after 3 attempts with empty responses'
 
       await expect(wrapper.authenticate()).to.be.rejectedWith(wrapper.RetryError, errorMessage
       )
     })
+
     it('Logs a RetryError when auth fails 3 times', async () => {
       const loggerError = sinon.spy(logger, 'error')
-      mockAxios.onPost(credsBase + 'token')
-        .reply(200, '')
+      wrapper = rewire('../index.js')
+      wrapper.config({ key: credsKey, secret: credsSecret, base: credsBase })
+      wrapper.__set__('fetch', (path, options) => {
+        return { status: 200, json: () => '' }
+      })
       const errorMessage = 'Authentication failed after 3 attempts with empty responses'
 
       await expect(wrapper.authenticate()).to.be.rejectedWith(wrapper.RetryError, errorMessage
@@ -88,31 +114,54 @@ describe('test', function () {
   })
 
   describe('generic get', () => {
-    beforeEach(() => {
-      mockAxios.onPost(credsBase + 'token')
-        .reply(200, { access_token: '12345' })
-    })
     const response = 'all of the books'
     it('returns data', async () => {
-      mockAxios.onGet(credsBase + 'spaghetti')
-        .reply(200, response)
+      wrapper = rewire('../index.js')
+      wrapper.config({ key: credsKey, secret: credsSecret, base: credsBase })
+      wrapper.__set__('fetch', (path, options) => {
+        if (path === credsBase + 'token' && options.headers.Authorization === 'Basic Y3JlZHNLZXk6Y3JlZHNTZWNyZXQ=') {
+          return { status: 200, json: () => { return { access_token: '12345' } } }
+        }
+        if (path === credsBase + 'spaghetti') {
+          return { status: 200, json: () => { return response } }
+        }
+      })
       expect(await wrapper.get('spaghetti')).to.equal(response)
     })
 
     it('retries get request 1x when given an empty response once', async () => {
-      const getCalls = mockAxios.onGet(credsBase + 'books')
-        .replyOnce(200, '')
-        .onGet(credsBase + 'books')
-        .replyOnce(200, response)
+      let tries = 0
 
-      await wrapper.get('books')
+      wrapper = rewire('../index.js')
+      wrapper.config({ key: credsKey, secret: credsSecret, base: credsBase })
+      wrapper.__set__('fetch', (path, options) => {
+        if (path === credsBase + 'token' && options.headers.Authorization === 'Basic Y3JlZHNLZXk6Y3JlZHNTZWNyZXQ=') {
+          return { status: 200, json: () => { return { access_token: '12345' } } }
+        }
+        if (path === credsBase + 'books' && tries > 0) {
+          return { status: 200, json: () => { return response } }
+        } else {
+          tries += 1
+          return { status: 200, json: () => '' }
+        }
+      })
 
-      expect(getCalls.history.get.length).to.equal(2)
+      const resp = await wrapper.get('books')
+
+      expect(tries).to.equal(1)
+      expect(resp).to.equal(response)
     })
 
     it('Throws a retry error when request is empty 3x', async () => {
-      mockAxios.onGet(credsBase + 'books')
-        .reply(200, '')
+      wrapper = rewire('../index.js')
+      wrapper.config({ key: credsKey, secret: credsSecret, base: credsBase })
+      wrapper.__set__('fetch', (path, options) => {
+        if (path === credsBase + 'token' && options.headers.Authorization === 'Basic Y3JlZHNLZXk6Y3JlZHNTZWNyZXQ=') {
+          return { status: 200, json: () => { return { access_token: '12345' } } }
+        } else {
+          return { status: 200, json: () => '' }
+        }
+      })
       const errorMessage = 'Get request failed after 3 attempts with empty responses'
       await expect(wrapper.get('books')).to.be.rejectedWith(wrapper.RetryError,
         errorMessage)
@@ -120,8 +169,15 @@ describe('test', function () {
 
     it('Logs a retry error when request is empty 3x', async () => {
       const loggerError = sinon.spy(logger, 'error')
-      mockAxios.onGet(credsBase + 'books')
-        .reply(200, '')
+      wrapper = rewire('../index.js')
+      wrapper.config({ key: credsKey, secret: credsSecret, base: credsBase })
+      wrapper.__set__('fetch', (path, options) => {
+        if (path === credsBase + 'token' && options.headers.Authorization === 'Basic Y3JlZHNLZXk6Y3JlZHNTZWNyZXQ=') {
+          return { status: 200, json: () => { return { access_token: '12345' } } }
+        } else {
+          return { status: 200, json: () => '' }
+        }
+      })
       const errorMessage = 'Get request failed after 3 attempts with empty responses'
       await expect(wrapper.get('books')).to.be.rejectedWith(wrapper.RetryError,
         errorMessage)
@@ -134,17 +190,23 @@ describe('test', function () {
     let handleAuthErrorSpy
     const path = 'path'
     const data = { books: 'the books' }
+    let tries = 0
     beforeEach(() => {
+      tries = 0
       const _handleAuthError = wrapper.__get__('_handleAuthError')
       handleAuthErrorSpy = sinon.spy(_handleAuthError)
       wrapper.__set__('_handleAuthError', handleAuthErrorSpy)
-      // mock successful auth call
-      mockAxios.onPost(`${credsBase}token`)
-        .reply(200, { access_token: '12345' })
-      // mock response to first tested request with expired token
-      mockAxios.onAny(credsBase + path).replyOnce(401)
-        // mock retry with a success
-        .onAny(credsBase + path).reply(200, 'success')
+      wrapper.config({ key: credsKey, secret: credsSecret, base: credsBase })
+      wrapper.__set__('fetch', (path, options) => {
+        if (path === credsBase + 'token' && options.headers.Authorization === 'Basic Y3JlZHNLZXk6Y3JlZHNTZWNyZXQ=') {
+          return { status: 200, json: () => { return { access_token: '12345' } } }
+        } else if (tries % 2 === 1) {
+          return { status: 200, json: () => { return 'success' } }
+        } else {
+          tries += 1
+          return { status: 401, json: () => '' }
+        }
+      })
     })
     after(() => { wrapper = requireUncached('../index.js') })
     it('post', async () => {
@@ -169,61 +231,77 @@ describe('test', function () {
     it('should recursively return all items from a given bib', async () => {
       const authStub = () => true
       wrapper.__set__('authenticate', authStub)
-      mockAxios
-        .onAny(credsBase + 'items/?bibIds=bibId&fields=default,fixedFields,varFields&offset=0').reply(200, { entries: Array(50).fill('item') })
-        .onAny(credsBase + 'items/?bibIds=bibId&fields=default,fixedFields,varFields&offset=50').reply(200, { entries: Array(50).fill('item') })
-        .onAny(credsBase + 'items/?bibIds=bibId&fields=default,fixedFields,varFields&offset=100').reply(200, { entries: Array(15).fill('item') })
+      wrapper.config({ key: credsKey, secret: credsSecret, base: credsBase })
+      wrapper.__set__('fetch', (path, options) => {
+        if (path === credsBase + 'token' && options.headers.Authorization === 'Basic Y3JlZHNLZXk6Y3JlZHNTZWNyZXQ=') {
+          return { status: 200, json: () => { return { access_token: '12345' } } }
+        } else if (path === credsBase + 'items/?bibIds=bibId&fields=default,fixedFields,varFields&offset=0') {
+          return { status: 200, json: () => { return { entries: Array(50).fill('item') } } }
+        } else if (path === credsBase + 'items/?bibIds=bibId&fields=default,fixedFields,varFields&offset=50') {
+          return { status: 200, json: () => { return { entries: Array(50).fill('item') } } }
+        } else if (path === credsBase + 'items/?bibIds=bibId&fields=default,fixedFields,varFields&offset=100') {
+          return { status: 200, json: () => { return { entries: Array(15).fill('item') } } }
+        }
+      })
 
       const items = await wrapper.getBibItems('bibId')
       expect(items.length).to.equal(115)
     })
   })
+
   describe('makes a request with the rights headers, method, path, and data', () => {
-    let axiosCalls
+    let fetchCalls
     const data = { title: 'spaghetti' }
     beforeEach(() => {
-      axiosCalls = mockAxios
-        .onPost(credsBase + 'token').reply(200, { access_token: '12345' })
-        .onAny(credsBase + 'path').reply(200, 'success')
+
+      fetchCalls = []
+      wrapper.config({ key: credsKey, secret: credsSecret, base: credsBase })
+      wrapper.__set__('fetch', (path, options) => {
+        fetchCalls.push({ path, options })
+        if (path === credsBase + 'token' && options.headers.Authorization === 'Basic Y3JlZHNLZXk6Y3JlZHNTZWNyZXQ=') {
+          return { status: 200, json: () => { return { access_token: '12345' } } }
+        } else {
+          return { status: 200, json: () => 'success' }
+        }
+      })
     })
     it('put', async () => {
       await wrapper.put('path', data)
 
-      const putCalls = axiosCalls.history.put
+      const putCalls = fetchCalls.filter(call => call.options.method === 'put')
       expect(putCalls.length).to.equal(1)
-      expect(putCalls[0].headers.Authorization).to.equal('Bearer 12345')
-      expect(putCalls[0].method).to.equal('put')
-      expect(putCalls[0].url).to.equal(credsBase + 'path')
-      expect(JSON.parse(putCalls[0].data)).to.deep.equal(data)
+      expect(putCalls[0].options.headers.Authorization).to.equal('Bearer 12345')
+      expect(putCalls[0].options.method).to.equal('put')
+      expect(putCalls[0].path).to.equal(credsBase + 'path')
+      expect(JSON.parse(putCalls[0].options.body)).to.deep.equal(data)
     })
     it('post', async () => {
       await wrapper.post('path', data)
 
-      const postCalls = axiosCalls.history.post
+      const postCalls = fetchCalls.filter(call => call.options.method === 'post')
       // 2 calls because the first one is auth post
       expect(postCalls.length).to.equal(2)
-      expect(postCalls[1].headers.Authorization).to.equal('Bearer 12345')
-      expect(postCalls[1].method).to.equal('post')
-      expect(postCalls[1].url).to.equal(credsBase + 'path')
-      expect(JSON.parse(postCalls[1].data)).to.deep.equal(data)
+      expect(postCalls[1].options.headers.Authorization).to.equal('Bearer 12345')
+      expect(postCalls[1].options.method).to.equal('post')
+      expect(postCalls[1].path).to.equal(credsBase + 'path')
+      expect(JSON.parse(postCalls[1].options.body)).to.deep.equal(data)
     })
     it('delete', async () => {
       await wrapper.deleteRequest('path')
-
-      const deleteCalls = axiosCalls.history.delete
+      const deleteCalls = fetchCalls.filter(call => call.options.method === 'delete')
       expect(deleteCalls.length).to.equal(1)
-      expect(deleteCalls[0].headers.Authorization).to.equal('Bearer 12345')
-      expect(deleteCalls[0].method).to.equal('delete')
-      expect(deleteCalls[0].url).to.equal(credsBase + 'path')
+      expect(deleteCalls[0].options.headers.Authorization).to.equal('Bearer 12345')
+      expect(deleteCalls[0].options.method).to.equal('delete')
+      expect(deleteCalls[0].path).to.equal(credsBase + 'path')
     })
     it('get', async () => {
       await wrapper.get('path')
 
-      const getCalls = axiosCalls.history.get
+      const getCalls = fetchCalls.filter(call => call.options.method === 'get')
       expect(getCalls.length).to.equal(1)
-      expect(getCalls[0].headers.Authorization).to.equal('Bearer 12345')
-      expect(getCalls[0].method).to.equal('get')
-      expect(getCalls[0].url).to.equal(credsBase + 'path')
+      expect(getCalls[0].options.headers.Authorization).to.equal('Bearer 12345')
+      expect(getCalls[0].options.method).to.equal('get')
+      expect(getCalls[0].path).to.equal(credsBase + 'path')
     })
   })
 })
